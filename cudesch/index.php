3b8c0a5928c12573751c2071f13db51b4870beae
Carlo Beltrame <carlo@beltra.me>
2023-12-19 17:46:27 +0100

    Display hierarchy for each found document

--- cudesch/index.php
+++ cudesch/index.php
@@ -123,6 +123,10 @@ usort($documents, function ($a, $b) { return $a['name'] <=> $b['name']; });
     content.classList.add('paper')
     details.appendChild(content)
 
+    const hierarchy = document.createElement('header')
+    hierarchy.innerText = entry.hierarchy.join(' → ')
+    content.appendChild(hierarchy)
+
     const scrollArea = document.createElement('article')
     scrollArea.innerHTML = marked.parse(entry.markdown)
     content.appendChild(scrollArea)
