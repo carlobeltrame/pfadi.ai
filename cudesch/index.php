b8390e06f99e5424e1c4945e44a3a6ab20eb3601
Carlo Beltrame <carlo@beltra.me>
2025-04-24 16:20:46 +0800

    Update terms of use

--- cudesch/index.php
+++ cudesch/index.php
@@ -266,7 +266,7 @@ usort($documents, function ($a, $b) { return $a['name'] <=> $b['name']; });
   Achtung: Für die Korrektheit und Relevanz der gefundenen Inhalte wird keinerlei Garantie übernommen. Die Texte sollten in jedem Fall noch von erfahrenen Kursleitenden gegengelesen werden. Diese Suche ist ein Experiment, wie weit die aktuellen AI-Tools bereits für Pfadi-Themen nutzbar sind.
 </p>
 <p><small>
-  Bitte keine personenbezogenen Daten in die Felder eingeben. Alle gefundenen Inhalte und dazugehörigen Eingaben werden beim Suchen zur Qualitätssicherung und Kontrolle der Kosten serverseitig gespeichert.
+  Bitte keine personenbezogenen Daten in die Felder eingeben. Alle gefundenen Inhalte und dazugehörigen Eingaben werden beim Suchen zur Qualitätssicherung, Kontrolle der Kosten und Weiterentwicklung der pfadi.ai Tools serverseitig gespeichert.
 </small></p>
 <p>
 <label for="literature_history_select">Bereits gefundene Literatur wieder aufrufen</label>
